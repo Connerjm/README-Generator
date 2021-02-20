beee25119bbe4412f8d346ef48b29c08454c6539
Conner Martin <connerjm2@gmail.com>
2021-02-19 18:38:28 -0800

    Added given files and created packages.

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import generate from "generate";
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
