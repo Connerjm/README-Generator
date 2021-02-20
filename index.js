cb9a1e9b073a921b2d669a4769af4a574b279a56
Conner Martin <connerjm2@gmail.com>
2021-02-19 21:16:21 -0800

    Starting to attempt to integrate optional questions.

--- index.js
+++ index.js
@@ -17,30 +17,43 @@ const questions = [
         type: "input",
         message: "Describe your project.",
         name: "description"
-    }
+    },
 ];
 
 /* Functions */
 
+function promptUser(sectionsArr)
+{
+    console.log(sectionsArr);
+    inquirer.prompt(questions)
+        .then((answers) => writeToFile("./NEWREADME.md", generate(answers)))
+        .catch(error => console.error(error));
+}
+
 //Writes the formatted data into the file.
 function writeToFile(fileName, data)
 {
-    fs.writeFile(fileName, data, (error) =>
-    {
-        if (error)
-            console.error(error);
-        else
-            console.log("Success!");
-    });
+    // fs.writeFile(fileName, data, (error) =>
+    // {
+    //     if (error)
+    //         console.error(error);
+    //     else
+    //         console.log("Success!");
+    // });
 }
 
 //Initializing function called when the application is run.
 function init()
 {
     inquirer
-        .prompt(questions)
-        .then((answers) => { writeToFile("./NEWREADME.md", generate(answers));})
-        .catch(error =>  { console.error(error); });
+        .prompt({
+            type: "checkbox",
+            message: "Which of these sections would you like to include?",
+            choices: ["Installation", "Usage", "License", "Contributing", "Tests", "Questions"],
+            name: "sections"
+        })
+        .then(sections => promptUser(sections.sections))
+        .catch(error => console.error(error))
 }
 
 /* Initialize */
